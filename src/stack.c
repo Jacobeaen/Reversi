6aa1f4069c8cfa25353e623880f829f3590b3ef4
Nikita Talalai <nikita.talalai.2002@gmail.com>
2025-08-13 23:16:52 +0300

    Fix bugs with bot and add levels

--- src/stack.c
+++ src/stack.c
@@ -1,60 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-typedef struct {
-    int cell_x[60];
-    int cell_y[60];
-    int size;
+typedef struct
+{
+   int cell_x[60];
+   int cell_y[60];
+   int size;
 } moves;
 
-typedef struct Stack {
-    struct Stack *next;
-    moves data;
+typedef struct Stack
+{
+   struct Stack *next;
+   moves data;
 } Stack;
 
+Stack *push(Stack *top, moves *data)
+{
+   Stack *new_top = (Stack *)malloc(sizeof(Stack));
 
-Stack* push(Stack *top, moves *data) {
-    Stack *new_top = (Stack *)malloc(sizeof(Stack));
+   if (new_top == NULL)
+   {
+      printf("Can't allocate memory!");
 
-    if (new_top == NULL){
-        printf("Can't allocate memory!");
+      return NULL;
+   }
 
-        return NULL;
-    }
+   new_top->data = *data;
+   new_top->next = top;
 
-    new_top->data = *data;
-    new_top->next = top;
-
-    return new_top;
+   return new_top;
 }
 
-Stack* pop(Stack *top, moves *data) {
-    if (top == NULL){
-        puts("You're at bottom of the stack");
-        
-        return NULL;
-    }
+Stack *pop(Stack *top, moves *data)
+{
+   if (top == NULL)
+   {
+      puts("You're at bottom of the stack");
 
-    *data = top->data;
-    
-    Stack *ptr = top->next;
-    free(top);
+      return NULL;
+   }
 
-    return ptr;
+   *data = top->data;
+
+   Stack *ptr = top->next;
+   free(top);
+
+   return ptr;
 }
 
-Stack* clear_stack(Stack *stack){
-    if (stack == NULL)
-        return NULL;
+Stack *clear_stack(Stack *stack)
+{
+   if (stack == NULL)
+      return NULL;
 
-    Stack *ptr = stack;
-    while (ptr != NULL){
-        Stack *next = ptr->next;
-        
-        free(ptr);
-        ptr = next;
-    }
+   Stack *ptr = stack;
+   while (ptr != NULL)
+   {
+      Stack *next = ptr->next;
 
-    return ptr;
+      free(ptr);
+      ptr = next;
+   }
+
+   return ptr;
 }
